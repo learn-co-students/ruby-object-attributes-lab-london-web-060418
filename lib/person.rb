036edaec5e8fc7019b93fe3d29c03475bd80940d
Hugo Sanchez <hugosanchez251@gmail.com>
2018-06-06 20:15:42 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(p_job)
+    @job = p_job
+  end
+  
+  def job
+    @job
+  end
+
+end
+
